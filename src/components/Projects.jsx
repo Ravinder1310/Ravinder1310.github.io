e5272e74a49f3630151c42f240a9a6bd0aaa7c24
Ravinder Kumar <105969256+Ravinder1310@users.noreply.github.com>
2023-09-25 10:29:50 +0530

    added a full-stack project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,39 +5,39 @@ import { Box, Button, Link } from '@chakra-ui/react';
 
 const projects_data = [
     {
-        video:"../images/projects/here_gig.gif",
-        title:"Nearbuy clone",
-        discription:"It is an e-commerce website clone of Nearbuy for best deals or offers related to resturants, salons or spas.",
-        github:'https://github.com/Ravinder1310/NearBuy-clone',
-        live:"https://soft-custard-9f29c0.netlify.app/",
-        stacks:["React | ","Javascript | ","CSS | ","Chakra UI | "],
-        responsibility:["HomePage | ", "Category page | ","Category Navbar"]
+        video:"../images/projects/p4.gif",
+        title: "U-Look(cosmetic products)",
+        description: "The U-Look website is an immersive online platform dedicated to beauty and skincare products.",
+        GitHub: 'https://github.com/Ravinder1310/ecommerce',
+        live: "https://ecommerce-app-bice-rho.vercel.app/",
+        stacks:["React | "," Redux | "," Javascript | ", "CSS | ", "Bootstrap | ", "NodeJs | ", "Express.js | ","JWT | ","MongoDb"],
+        responsibility:["Complete website"]
     },
     {
-        video:"../images/projects/indiaToday_gif.gif",
-        title:"IndiaToday clone",
-        discription:"A news website clone of IndiaToday for the latest news around the world.",
-        github:'https://github.com/Ravinder1310/IndiaToday-clone',
-        live:"https://bespoke-naiad-451ef2.netlify.app/index.html",
-        stacks:["HTML | ","Javascript | ","CSS | "],
-        responsibility:["Login page | ", "SignUp page | ","Category Section"]
+        video:"../images/projects/here_gig.gif",
+        title: "Nearbuy clone",
+        description: "It is an e-commerce website clone of Nearbuy for best deals or offers related to restaurants, salons, or spas.",
+        GitHub:'https://github.com/Ravinder1310/NearBuy-clone',
+        live: "https://soft-custard-9f29c0.netlify.app/",
+        stacks:["React | "," Javascript | ", "CSS | ", "Chakra UI | "],
+        responsibility:["HomePage | ", "Category page | ", "Category Navbar"]
     },
     {
         video:"../images/projects/wrike_gif.gif",
-        title:"Wrike clone",
-        discription:"The most powerfull work management software clone by which we can manage our projects in a team.",
-        github:'https://github.com/Ravinder1310/Wrike-clone',
-        live:"https://earnest-biscuit-2c12ec.netlify.app/",
+        title: "Wrike clone",
+        description: "The most powerfull work management software clone by which we can manage our projects in a team.",
+        GitHub:'https://github.com/Ravinder1310/Wrike-clone',
+        live: "https://earnest-biscuit-2c12ec.netlify.app/",
         stacks:["HTML | ","Javascript | ","CSS | "],
         responsibility:["HomePage | ", "Product pages | ","Login Page | ","SignUp page"]
     },
     {
         video:"../images/projects/here_gig.gif",
-        title:"ZARA clone",
-        discription:"This is one of the biggest international fashion companies, and it belongs to Inditex, one of the world’s largest distribution groups.",
-        github:'https://github.com/Ravinder1310/Zara-clone',
-        live:"https://quiet-begonia-859735.netlify.app/",
-        stacks:["HTML | ","Javascript | ","CSS | "],
+        title: "ZARA clone",
+        description: "This is one of the biggest international fashion companies, and it belongs to Inditex, one of the world’s largest distribution groups.",
+        GitHub:'https://github.com/Ravinder1310/Zara-clone',
+        live: "https://quiet-begonia-859735.netlify.app/",
+        stacks:["HTML | ", "Javascript | ", "CSS | "],
         responsibility:["Navbar | ", "Product pages | "]
     }
 ]
@@ -47,7 +47,7 @@ export const Projects = () => {
 
     return ( 
         <Box w={"60%"} margin="auto" mt={"250px"} id="Project">
-            <h1 style={{fontSize:"30px",color:"white",textDecoration:"underline",textDecorationColor:"red",margin:"auto",marginBottom:"30px"}}>Projects</h1>
+            <h1 style={{fontSize: "30px", color:"white",textDecoration:"underline",textDecorationColor:"red",margin:"auto",marginBottom:"30px"}}>Projects</h1>
             <Box className="Proj_videos">
             {
                 projects_data?.map(({video,title,discription,github,live,stacks,responsibility},index)=>{
